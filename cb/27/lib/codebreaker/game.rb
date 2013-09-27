8e59dc71f89aaa44e40c2dafbe98282fb06e4505
Michihito Shigemura <i.am.shige@gmail.com>
2013-09-28 02:29:08 +0900

    P70 1 match

---
+++ cb/27/lib/codebreaker/game.rb
@@ -0,0 +1,20 @@
+module Codebreaker
+  class Game
+    def initialize(output)
+      @output = output
+    end
+
+    def start(secret)
+      @secret = secret
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
+    end
+
+    def guess(guess)
+      if @secret.include?(guess[0])
+        @output.puts '-'
+      end
+      @output.puts ''
+    end
+  end
+end
