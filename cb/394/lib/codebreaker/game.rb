7221a94c0ce0adb3441a4b138eb00bfe93677b8d
Michihito Shigemura <i.am.shige@gmail.com>
2013-09-28 21:01:59 +0900

    P84 refactor number_match_count

--- cb/394/lib/codebreaker/game.rb
+++ cb/394/lib/codebreaker/game.rb
@@ -11,14 +11,8 @@ module Codebreaker
     end
 
     def guess(guess)
-      number_match_count = 0
       mark = ''
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
-      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count
+      @output.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
     end
 
     def exact_match_count(guess)
@@ -29,6 +23,16 @@ module Codebreaker
         end
       end
       exact_match_count
+    end
+
+    def number_match_count(guess)
+      number_match_count = 0
+      (0..3).each do |index|
+        if number_match?(guess, index)
+          number_match_count += 1
+        end
+      end
+      number_match_count
     end
 
     def exact_match?(guess, index)
